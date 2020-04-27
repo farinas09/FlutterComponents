3e58ec0742582b83ae98850979c1ec54750397db
efarinas <efarinas99@gmail.com>
2020-04-27 17:25:29 -0600

    adding image holder

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -10,7 +10,9 @@ class CardPage extends StatelessWidget {
       body: ListView(
         padding: EdgeInsets.all(6.0),
         children: <Widget> [
-          _cardTipo1()
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
         ],
       ),
     );
@@ -29,15 +31,38 @@ class CardPage extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
-              FlatButton(onPressed: () {
-
-              },
-              child: Text("Okay"))
+              FlatButton(onPressed: () {},
+              child: Text("Cancelar")),
+              FlatButton(onPressed: () {},
+              child: Text("Aceptar")),
             ],
           ),
         ]
       ),
     );
 
+  }
+
+  _cardTipo2() {
+
+    return Card(
+      child: Column(
+        children: <Widget> [
+
+          FadeInImage(
+            placeholder: AssetImage('assets/loading.gif'),
+            image: NetworkImage('https://unsplash.com/photos/twukN12EN7c/download'),
+            fadeInDuration: Duration(milliseconds: 100),
+            height: 250.0,
+            ),
+          //Image(
+            //image: NetworkImage('https://unsplash.com/photos/twukN12EN7c/download'),
+          //),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text("Ejemplo")),
+        ],
+      ),
+    );
   }
 }
